061cacbb6d6a4670db3d38faefeab32370b74235
Aaron J. Smith <onsmith@cs.unc.edu>
2018-03-09 18:20:08 -0500

    Clean up python script; output in kbps instead of raw bytes; other changes

--- python/hevc_bitrate_count.py
+++ python/hevc_bitrate_count.py
@@ -12,29 +12,29 @@ import re
 ################################################################################
 
 ## Path of source video file
-source_video_path = "tractor.mp4"
+source_video_path = "jellyfish.mp4"
 
 
 ## Bitrates to test (in kbps)
-#bitrates = [
-#	"100000k",
-#	"80000k",
-#	"60000k",
-#	"40000k",
-#	"20000k",
-#	"10000k",
-#	"5000k",
-#	"1000k",
-#	"500k",
-#	"250k"
-#]
 bitrates = [
-	"400k",
-	"375k",
-	"350k",
-	"325k",
-	"300k"
+	"100000k",
+	"80000k",
+	"60000k",
+	"40000k",
+	"20000k",
+	"10000k",
+	"5000k",
+	"1000k",
+	"500k",
+	"250k"
 ]
+#bitrates = [
+#	"400k",
+#	"375k",
+#	"350k",
+#	"325k",
+#	"300k"
+#]
 
 
 ## If true, the recoded videos will be saved in the current directory
@@ -110,56 +110,153 @@ extracted_bitstream_file_name = "recoded.h265"
 psnr_output_file_name = "psnr.mp4"
 
 
-## Command line input to extract hevc bitstream
-extract_hevc_bitstream = [
-	"ffmpeg",
-	"-i", recoded_video_file_name,
-	"-c:v", "copy",
-	"-bsf", "hevc_mp4toannexb",
-	extracted_bitstream_file_name
-]
-
-
-## Command line input to run the bit count decoder
-bit_count_decoder = [
-	bit_count_decoder_path,
-	"-b", extracted_bitstream_file_name
-]
-
-
-## Command line input to calculate psnr
-calculate_psnr = [
-	"ffmpeg",
-	"-i", source_video_path,
-	"-i", recoded_video_file_name,
-	"-filter_complex", "psnr",
-	psnr_output_file_name
-]
-
-
 ## CABAC line parsing regex
 cabac_regex = re.compile(
-	"^\s(\S+)\s+:\s+(\S+)\s+(\S+)\s+(-?\d+)\s+(-?\d+)\s+(-?\d+)\s+(-?\d+)\s+(-?\d+)\s+(-?\d+)\s+(-?\d+)\s\(\s+(-?\d+)\)\s$"
+	"^\s(?P<syntax_element>\S+)\s+:\s+(\S+)\s+(\S+)\s+(-?\d+)\s+(-?\d+)\s+(-?\d+)\s+(-?\d+)\s+(-?\d+)\s+(-?\d+)\s+(?P<total_bits>-?\d+)\s\(\s+(-?\d+)\)\s$"
 )
 
 
 ## CAVLC line parsing regex
 cavlc_regex = re.compile(
-	"^\s(\S+)\s+:\s+-\s+-\s+(-?\d+)\s+(-?\d+)\s+(-?\d+)\s+(-?\d+)\s\(\s+(-?\d+)\)\s$"
+	"^\s(?P<syntax_element>\S+)\s+:\s+-\s+-\s+(-?\d+)\s+(-?\d+)\s+(-?\d+)\s+(?P<total_bits>-?\d+)\s\(\s+(-?\d+)\)\s$"
 )
 
 
 ## PSNR parsing regex
 psnr_regex = re.compile(
-	"PSNR y:(\d+\.?\d*) u:(\d+\.?\d*) v:(\d+\.?\d*) average:(\d+\.?\d*) min:(\d+\.?\d*) max:(\d+\.?\d*)"
+	"PSNR y:(?P<y>\d+\.?\d*) u:(?P<u>\d+\.?\d*) v:(?P<v>\d+\.?\d*) average:(?P<avg>\d+\.?\d*) min:(?P<min>\d+\.?\d*) max:(?P<max>\d+\.?\d*)"
+)
+
+
+## FPS parsing regex
+fps_regex = re.compile(
+	"hevc:fps=(?P<fps>\d+)"
 )
 
 
 ## Stats parsing regex
 stats_regex = re.compile(
-	"encoded (\d+) frames? in \d+\.?\d*s \((\d+\.?\d*) fps\), (\d+\.?\d*) kb/s, Avg QP:(\d+\.?\d*)"
+	"encoded (?P<frames>\d+) frames? in \d+\.?\d*s \(\d+\.?\d* fps\), \d+\.?\d* kb/s, Avg QP:(?P<qp>\d+\.?\d*)"
 )
 
+
+## Frame stats parsing regexes
+iframes_regex = re.compile(
+	"x265 [info]: frame I:\s*(?P<count>\d+),\s*Avg QP:\s*(?P<qp>\d+\.?\d*)\s*kb/s:\s*(?P<kbps>\d+\.?\d*)"
+)
+pframes_regex = re.compile(
+	"x265 [info]: frame P:\s*(?P<count>\d+),\s*Avg QP:\s*(?P<qp>\d+\.?\d*)\s*kb/s:\s*(?P<kbps>\d+\.?\d*)"
+)
+bframes_regex = re.compile(
+	"x265 [info]: frame B:\s*(?P<count>\d+),\s*Avg QP:\s*(?P<qp>\d+\.?\d*)\s*kb/s:\s*(?P<kbps>\d+\.?\d*)"
+)
+
+
+
+
+################################################################################
+#
+# Methods
+#
+################################################################################
+
+## Prints a results data structure to stdout
+def print_results(data):
+	seconds = data['frames'] / data['fps']
+	print("\t".join([
+		bitrate[:-1],
+		str(data['bitstream_size'] * 8 / 1000 / seconds),
+		str(data['prediction'] / 1000 / seconds),
+		str(data['residual'] / 1000 / seconds),
+		str(data['other'] / 1000 / seconds),
+		str(data['psnr']),
+		str(data['qp'])
+	]))
+
+
+## Tries to delete any file that this script may have created; fails silently
+##   for files which can't be deleted
+def clean_up_files(bitrate):
+	try:
+		os.remove("x265_2pass.log")
+	except:
+		pass
+	
+	try:
+		os.remove("x265_2pass.log.cutree")
+	except:
+		pass
+	
+	try:
+		os.remove(psnr_output_file_name)
+	except:
+		pass
+	
+	try:
+		os.remove(extracted_bitstream_file_name)
+	except:
+		pass
+	
+	try:
+		if save_recoded_videos:
+			os.rename(recoded_video_file_name, bitrate + "_" + recoded_video_file_name)
+		else:
+			os.remove(recoded_video_file_name)
+	except:
+		pass
+
+
+## Performs two-pass encoding via x265 with ffmpeg to recode a given video file,
+##   achieving a target bitrate. Returns the ffmpeg output text
+##
+## See https://trac.ffmpeg.org/wiki/Encode/H.265#Two-PassExample
+def recode_video(video, bitrate):
+	subprocess.run([
+		"ffmpeg",
+		"-y",
+		"-i", video,
+		"-c:v", "libx265",
+		"-b:v", bitrate,
+		"-x265-params", "pass=1",
+		"-c:a", "aac",
+		"-b:a", "128k",
+		"-f", "mp4",
+		"NUL" # "NUL" on Windows, "/dev/null" on linux
+	], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+	
+	return subprocess.run([
+		"ffmpeg",
+		"-i", video,
+		"-c:v", "libx265",
+		"-b:v", bitrate,
+		"-x265-params", "pass=2",
+		"-c:a", "aac",
+		"-b:a", "128k",
+		recoded_video_file_name
+	], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
+## Runs ffmpeg on two versions of a video to calculate the psnr between the
+##   videos.
+def calculate_psnr(vid1, vid2):
+	return subprocess.run([
+		"ffmpeg",
+		"-i", vid1,
+		"-i", vid2,
+		"-filter_complex", "psnr",
+		psnr_output_file_name
+	], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
+## Runs ffmpeg to extract the raw hevc bitstream from a video file
+def extract_hevc_bitstream(mp4_video_in, hevc_bitstream_out):
+	return subprocess.run([
+		"ffmpeg",
+		"-i", mp4_video_in,
+		"-c:v", "copy",
+		"-bsf", "hevc_mp4toannexb",
+		hevc_bitstream_out
+	], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 
 
@@ -174,135 +271,99 @@ results = {}
 
 
 ## Print header
-print("\t".join(["total", "prediction", "residual", "other", "psnr", "qp"]))
+print("\t".join([
+	"target_bitrate_kbps",
+	"achieved_bitrate_kbps",
+	"prediction_kbps",
+	"residual_kbps",
+	"other_kbps",
+	"avg_psnr",
+	"avg_qp"
+]))
 
 
 ## Bitrate loop
 for bitrate in bitrates:
-	## Initialize entry in results dictionary
-	results[bitrate] = {
-		'prediction': 0,
-		'residual': 0,
-		'other': 0
-	}
+	try:
+		## Initialize entry in results dictionary
+		results[bitrate] = {
+			'prediction': 0,
+			'residual': 0,
+			'other': 0
+		}
+		
+		
+		## Recode video to target bitrate
+		result = recode_video(source_video_path, bitrate)
+		output = result.stderr.decode('utf-8')
+		
+		
+		## Capture average qp and number of frames
+		stats_match = stats_regex.search(output)
+		results[bitrate]['frames'] = int(stats_match.group("frames"))
+		results[bitrate]['qp']     = float(stats_match.group("qp"))
+		
+		
+		## Run PSNR / frame rate calculation
+		result = calculate_psnr(source_video_path, recoded_video_file_name)
+		output = result.stderr.decode('utf-8')
+		
+		
+		## Capture PSNR
+		psnr_match = psnr_regex.search(output)
+		results[bitrate]['psnr'] = float(psnr_match.group("avg"))
+		
+		
+		## Capture frame rate
+		fps_match = fps_regex.search(output)
+		results[bitrate]['fps'] = int(fps_match.group("fps"))
+		
+		
+		## Extract raw h265 bitstream from video container format
+		extract_hevc_bitstream(recoded_video_file_name, extracted_bitstream_file_name)
+		
+		
+		## Save hevc bitstream size
+		results[bitrate]['bitstream_size'] = os.path.getsize(extracted_bitstream_file_name)
+		
+		
+		## Run bit count decoder
+		result = subprocess.run([
+			bit_count_decoder_path,
+			"-b", extracted_bitstream_file_name
+		], stdout=subprocess.PIPE)
+		output = result.stdout.decode('utf-8')
+		lines  = output.splitlines()
+		
+		
+		## Parse bit count output line-by-line
+		for line in lines:
+			cabac_match = cabac_regex.match(line)
+			cavlc_match = cavlc_regex.match(line)
+			if cabac_match:
+				if cabac_match.group('syntax_element') in excluded_keys:
+					pass
+				elif cabac_match.group('syntax_element') in prediction_keys:
+					results[bitrate]['prediction'] += int(cabac_match.group('total_bits'))
+				elif cabac_match.group('syntax_element') in residual_keys:
+					results[bitrate]['residual'] += int(cabac_match.group('total_bits'))
+				else:
+					results[bitrate]['other'] += int(cabac_match.group('total_bits'))
+			elif cavlc_match:
+				if cavlc_match.group('syntax_element') in excluded_keys:
+					pass
+				if cavlc_match.group('syntax_element') in prediction_keys:
+					results[bitrate]['prediction'] += int(cavlc_match.group('total_bits'))
+				elif cavlc_match.group('syntax_element') in residual_keys:
+					results[bitrate]['residual'] += int(cavlc_match.group('total_bits'))
+				else:
+					results[bitrate]['other'] += int(cavlc_match.group('total_bits'))
+		
+		
+		## Print results for this bitrate
+		print_results(results[bitrate])
 	
 	
-	## Recode video to target bitrate (2-pass encoding)
-	## See https://trac.ffmpeg.org/wiki/Encode/H.265#Two-PassExample
-	subprocess.run([
-		"ffmpeg",
-		"-y",
-		"-i", source_video_path,
-		"-c:v", "libx265",
-		"-b:v", bitrate,
-		"-x265-params", "pass=1",
-		"-c:a", "aac",
-		"-b:a", "128k",
-		"-f", "mp4",
-		"NUL" # "NUL" on Windows, "/dev/null" on linux
-	], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-	
-	
-	## Second encoding pass
-	result = subprocess.run([
-		"ffmpeg",
-		"-i", source_video_path,
-		"-c:v", "libx265",
-		"-b:v", bitrate,
-		"-x265-params", "pass=2",
-		"-c:a", "aac",
-		"-b:a", "128k",
-		recoded_video_file_name
-	], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	
-	
-	## Capture encoding statistics
-	output = result.stderr.decode('utf-8')
-	stats_match = stats_regex.search(output)
-	results[bitrate]['frame_count'] = int(stats_match.group(1))
-	results[bitrate]['fps']         = float(stats_match.group(2))
-	results[bitrate]['kbps']        = float(stats_match.group(3))
-	results[bitrate]['qp']          = float(stats_match.group(4))
-	
-	
-	## Clean files
-	os.remove("x265_2pass.log")
-	os.remove("x265_2pass.log.cutree")
-	
-	
-	## Calculate PSNR
-	result = subprocess.run(calculate_psnr, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	output = result.stderr.decode('utf-8')
-	psnr_match = psnr_regex.search(output)
-	results[bitrate]['psnr'] = float(psnr_match.group(4))
-	
-	
-	## Clean files
-	os.remove(psnr_output_file_name)
-	
-	
-	## Extract raw h265 bitstream from video container format
-	subprocess.run(extract_hevc_bitstream, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-	
-	
-	## Clean files
-	if save_recoded_videos:
-		os.rename(recoded_video_file_name, bitrate + "_" + recoded_video_file_name)
-	else:
-		os.remove(recoded_video_file_name)
-	
-	
-	## Save hevc bitstream size
-	results[bitrate]['hevc_bistream_size'] = os.path.getsize(extracted_bitstream_file_name)
-	
-	
-	## Run bit count decoder
-	result = subprocess.run(bit_count_decoder, stdout=subprocess.PIPE)
-	output = result.stdout.decode('utf-8')
-	lines  = output.splitlines()
-	
-	
-	## Clean files
-	os.remove(extracted_bitstream_file_name)
-	
-	
-	## Parse bit count output line-by-line
-	for line in lines:
-		cabac_match = cabac_regex.match(line)
-		cavlc_match = cavlc_regex.match(line)
-		if cabac_match:
-			if cabac_match.group(1) in excluded_keys:
-				pass
-			elif cabac_match.group(1) in prediction_keys:
-				results[bitrate]['prediction'] += int(cabac_match.group(10))
-			elif cabac_match.group(1) in residual_keys:
-				results[bitrate]['residual'] += int(cabac_match.group(10))
-			else:
-				results[bitrate]['other'] += int(cabac_match.group(10))
-		elif cavlc_match:
-			if cavlc_match.group(1) in excluded_keys:
-				pass
-			if cavlc_match.group(1) in prediction_keys:
-				results[bitrate]['prediction'] += int(cavlc_match.group(5))
-			elif cavlc_match.group(1) in residual_keys:
-				results[bitrate]['residual'] += int(cavlc_match.group(5))
-			else:
-				results[bitrate]['other'] += int(cavlc_match.group(5))
-	
-	
-	## Convert accumulated bits to bytes
-	results[bitrate]['prediction'] /= 8
-	results[bitrate]['residual']   /= 8
-	results[bitrate]['other']      /= 8
-	
-	
-	## Output results
-	#pprint.pprint({ bitrate: results[bitrate] })
-	print("\t".join([
-		str(results[bitrate]['hevc_bistream_size']),
-		str(results[bitrate]['prediction']),
-		str(results[bitrate]['residual']),
-		str(results[bitrate]['other']),
-		str(results[bitrate]['psnr']),
-		str(results[bitrate]['qp'])
-	]))
+	## Do our best to clean files
+	finally:
+		clean_up_files(bitrate)
